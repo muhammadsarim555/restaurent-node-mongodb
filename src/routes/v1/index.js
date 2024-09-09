04be902e50f6bbc97f4c0b7fbbdeab077539f59e
Muhammad Sarim <36129452+muhammadsarim555@users.noreply.github.com>
2024-09-10 01:15:08 +0500

    added middleware for users

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -12,12 +12,14 @@ const {
   createProduct,
 } = require("../../controllers/productController");
 
+const middleware = require("../../middleware/auth");
+
 const router = express.Router();
 
 router.post("/user/signup", signUp);
 router.post("/user/login", login);
 router.post("/create-category", createCategory);
-router.get("/get-all-categories", getCategories);
+router.get("/get-all-categories", middleware, getCategories);
 router.post("/create-product", createProduct);
 router.get("/get-product-from-category/:categoryId", getProductFromCategory);
 router.get("/get-feature-product", getFeatureProducts);
